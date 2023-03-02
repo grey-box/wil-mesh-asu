8fe9da1132fc8ff24a74e4b99cbc749ca28434f2
jgarvey-greybox <johngarvey1989@gmail.com>
2023-03-02 04:42:46 -0700

    jsgarvey Task#208 update UI for networking

--- Task#208/app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
+++ Task#208/app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.greyboxapp;
+package com.example.myapplication;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.greyboxapp", appContext.getPackageName());
+        assertEquals("com.example.myapplication", appContext.getPackageName());
     }
 }
--- Task#208/app/src/test/java/com/example/myapplication/ExampleUnitTest.java
+++ Task#208/app/src/test/java/com/example/myapplication/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.greyboxapp;
+package com.example.myapplication;
 
 import org.junit.Test;
 
