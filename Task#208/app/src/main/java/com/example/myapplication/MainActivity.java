8cff730532a5b3ed94b15e02d89df9a9a2d1ad94
John G <johngarvey1989@gmail.com>
2023-03-04 04:01:45 -0700

    JSGARVEY TASK#204 implement Broadcast Receiver

--- Task#208/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Task#208/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.IntentFilter;
 import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +26,12 @@ public class MainActivity extends AppCompatActivity {
 
     //JSGARVEY Create Wifi Manager
     WifiManager wifiManager;
+    //JSGARVEY
+    WifiP2pManager mManager;
+    WifiP2pManager.Channel mChannel;
+
+    BroadcastReceiver mReceiver;
+    IntentFilter mIntentFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,16 +44,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void exqListener(){
 
-        if(wifiManager.isWifiEnabled()){
-            btnOnOff.setText("WIFI ON");
-        }else{
-            btnOnOff.setText("WIFI OFF");
-        }
-
+        //Android no longer allows app automation to turn wifi on or off
         btnOnOff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 if(wifiManager.isWifiEnabled()){
                     wifiManager.setWifiEnabled(false);
                     btnOnOff.setText("WIFI OFF");
@@ -68,6 +72,29 @@ public class MainActivity extends AppCompatActivity {
         //JSGARVEY
         wifiManager= (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
 
+        //JSGARVEY
+        mManager = (WifiP2pManager) getApplicationContext().getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, getMainLooper(),null);
 
+        mReceiver = new WifiDirectBroadcastReceiver(mManager, mChannel, this);
+
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
     }
+
+    @Override
+    protected  void onResume(){
+        super.onResume();
+        registerReceiver(mReceiver,mIntentFilter);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
+
 }
