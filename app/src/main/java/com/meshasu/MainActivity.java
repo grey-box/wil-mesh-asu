026bd96f6743b669e90c18fbc3b2f831eb9c719e
Conner Duncan <conner.duncan@grey-box.ca>
2023-03-04 18:12:50 -0700

    Add intents

--- app/src/main/java/com/meshasu/MainActivity.java
+++ app/src/main/java/com/meshasu/MainActivity.java
@@ -1,5 +1,6 @@
 package com.meshasu;
 
+import android.content.IntentFilter;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.content.Context;
 import android.os.Bundle;
@@ -8,12 +9,20 @@ import android.app.Activity;
 public class MainActivity extends Activity implements WifiP2pManager.ChannelListener {
     WifiP2pManager manager;
     WifiP2pManager.Channel netChannel;
+    IntentFilter intentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         manager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
         netChannel = manager.initialize(this,getMainLooper(),null);
         //TODO: Initialize receiver here
+
+        intentManager = new IntentFilter();
+        intentManager.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        intentManager.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+        intentManager.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        intentManager.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
     }
 
     @Override
