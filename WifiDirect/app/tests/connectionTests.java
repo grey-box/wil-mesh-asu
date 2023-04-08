a26d16a64c609e31c180d4594bc88ca293473354
Conner Duncan <conner.duncan@ymail.com>
2023-04-08 13:29:49 -0700

    Create connectionTests.java
    Create a framework for the junit test for disconnection testing

---
+++ WifiDirect/app/tests/connectionTests.java
@@ -0,0 +1,45 @@
+class connectionTest {
+
+    @BeforeTest
+    setupBeforeEachTest() {
+        
+    }
+
+    nodePatternStraight() {
+
+    }
+
+    nodePatternWeb() {
+
+    }
+
+    @test
+    disconnectionPattern_1outOf6() {
+
+    }
+
+    @test
+    disconnectionPattern_2outOf6() {
+
+    }
+
+    @test
+    disconnectionPattern_3outOf6() {
+
+    }
+
+    @test
+    disconnectionPattern_4outOf6() {
+
+    }
+
+    @test
+    disconnectionPattern_5outOf6() {
+
+    }
+
+    @test
+    disconnectionPattern_6outOf6() {
+
+    }
+}
